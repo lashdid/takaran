031665d5d69ce9f8c6055ed406c81af4913d71d7
Lashdid <lashdid19@gmail.com>
2022-11-28 11:00:47 +0700

    input only number

--- src/lib/calc/Input.svelte
+++ src/lib/calc/Input.svelte
@@ -1,6 +1,13 @@
 <script>
-    export let placeholder;
-    export let value;
+  export let placeholder;
+  export let value;
 </script>
 
-<input type="number" class="rounded font-semibold outline-transparent border border-pink-300 w-full md:h-full p-3 py-[14px] placeholder-pink-300 transition text-pink-600 duration-300 focus:(placeholder-transparent)" placeholder={placeholder} bind:value={value} on:input/>
+<input
+  on:keypress={(event) => !/[0-9.]/.test(event.key) && event.preventDefault()}
+  type="number"
+  class="rounded font-semibold outline-transparent border border-pink-300 w-full md:h-full p-3 py-[14px] placeholder-pink-300 transition text-pink-600 duration-300 focus:(placeholder-transparent) focus:(border border-pink-400)"
+  {placeholder}
+  bind:value
+  on:input
+/>
